a83f3dedfbd78494ce400567340af31ad18a7417
徐振洋 <xzy20010911@163.com>
2026-09-03 13:25:25 +0800

    feat: add Shanghai as 6th city after Beijing

--- script.js
+++ script.js
@@ -61,6 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
         { name: 'hefei',     center: [117.23, 31.82], zoom: 12.0, pitch: 65, bearing: 0 },
         { name: 'hongkong',  center: [114.17, 22.32], zoom: 12.0, pitch: 65, bearing: 0 },
         { name: 'beijing',   center: [116.40, 39.90], zoom: 12.0, pitch: 65, bearing: 0 },
+        { name: 'shanghai',  center: [121.47, 31.23], zoom: 12.0, pitch: 65, bearing: 0 },
         { name: 'stars',     center: [105.0, 35.0], zoom: 0.0,  pitch: 0,  bearing: 0 },
     ];
     
